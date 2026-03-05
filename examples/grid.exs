fbc3d7f53e9afff27d1bc81f3c2bfdc647a6c4a7
Gary Rennie <gazler@gmail.com>
2026-03-05 16:40:12 +0000

    feat(Breeze.Renderer): support grid with apply_style
    BackBreeze now has grid support, this change provides an interface to
    the grid with `apply_style`.

---
+++ examples/grid.exs
@@ -0,0 +1,26 @@
+defmodule Demo do
+  use Breeze.View
+
+  def mount(_opts, term), do: {:ok, term}
+
+  def render(assigns) do
+    ~H"""
+    <box style="grid grid-cols-2 grid-rows-3 width-screen height-screen">
+      <box style="border border-1">Top Left</box>
+      <box style="border border-2">Top Right</box>
+      <box style="border border-3">Middle Left</box>
+      <box style="border border-4">Middle Right</box>
+      <box style="border border-5">Bottom Left</box>
+      <box style="border border-6">Bottom Right</box>
+    </box>
+    """
+  end
+
+  def handle_event(_, %{"key" => "q"}, term), do: {:stop, term}
+  def handle_event(_, _, term), do: {:noreply, term}
+end
+
+Breeze.Server.start_link(view: Demo)
+
+receive do
+end
