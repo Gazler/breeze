2c715564b57597155df3fd516e864410cd4298bb
Gary Rennie <gazler@gmail.com>
2026-07-30 15:12:22 +0100

    chore(deps): update back_breeze to 0.4.2

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Breeze.MixProject do
   defp deps do
     [
       {:termite, "~> 0.4 or ~> 0.5"},
-      {:back_breeze, github: "gazler/back_breeze"},
+      {:back_breeze, "~> 0.4.2"},
       {:file_system, "~> 1.1", optional: true, runtime: Mix.env() == :dev},
       {:telemetry, "~> 1.0"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
